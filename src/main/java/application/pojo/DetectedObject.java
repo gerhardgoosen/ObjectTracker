e979e51a736d335cc6d1ab7d522354b9c440f1bd
Gerhard Goosen <gerhard@loyaltyplus.co.za>
2019-05-15 21:56:24 +0200

    fine tne colors

--- src/main/java/application/pojo/DetectedObject.java
+++ src/main/java/application/pojo/DetectedObject.java
@@ -28,54 +28,60 @@ public class DetectedObject {
 			//TODO: use "calibration mode" to find HSV min
 			//and HSV max values
 
+
+//			setHSVmin(new Scalar(80,52.6,0));
+//			setHSVmax(new Scalar(128.5,254,255));
+
+			setHSVmin(new Scalar(75,194,31));
+			setHSVmax(new Scalar(107,256,256));
+
+			//BGR value for Blue:
+			setColor( new Scalar(255,0,0));
+			setFillColor(new Scalar(255,0,0,0.5));
+
+		}
+
+
+//		if(name=="blue"){
+//
+//			//TODO: use "calibration mode" to find HSV min
+//			//and HSV max values
+//
 //			setHSVmin(new Scalar(92,0,0));
 //			setHSVmax(new Scalar(124,256,256));
+//
+//			//BGR value for Blue:
+//			setColor(new Scalar(255,0,0));
+//			setFillColor(new Scalar(255,0,0,0.5));
+//
+//		}
 
-			setHSVmin(new Scalar(80,52.6,0));
-			setHSVmax(new Scalar(128.5,254,255));
-
-			//BGR value for Blue:
-			setColor(new Scalar(255,0,0));
-			setFillColor(new Scalar(255,0,0,0.5));
-
-		}
-
-
-		if(name=="blue"){
-
-			//TODO: use "calibration mode" to find HSV min
-			//and HSV max values
-
-			setHSVmin(new Scalar(92,0,0));
-			setHSVmax(new Scalar(124,256,256));
-
-			//BGR value for Blue:
-			setColor(new Scalar(255,0,0));
-			setFillColor(new Scalar(255,0,0,0.5));
-
-		}
 		if(name=="green"){
 
 			//TODO: use "calibration mode" to find HSV min
 			//and HSV max values
 
-			setHSVmin(new Scalar(34,50,50));
-			setHSVmax(new Scalar(80,220,200));
+//			setHSVmin(new Scalar(34,50,50));
+//			setHSVmax(new Scalar(80,220,200));
+
+			setHSVmin(new Scalar(55,80,7));
+			setHSVmax(new Scalar(87,176,256));
 
 			//BGR value for Green:
 			setColor(new Scalar(0,255,0));
 			setFillColor(new Scalar(0,255,0,50));
 
 		}
+
 		if(name=="yellow"){
 
 			//TODO: use "calibration mode" to find HSV min
 			//and HSV max values
 
 		    //setHSVmin(new Scalar(20,124,123));
-			setHSVmin(new Scalar(20,60,123));
-
 			//setHSVmax(new Scalar(30,256,256));
+
+			setHSVmin(new Scalar(20,60,123));
 			setHSVmax(new Scalar(50,200,256));
 
 			//BGR value for Yellow:
@@ -83,13 +89,17 @@ public class DetectedObject {
 			setFillColor(new Scalar(0,255,255,0));
 
 		}
+
 		if(name=="red"){
 
 			//TODO: use "calibration mode" to find HSV min
 			//and HSV max values
 
-			setHSVmin(new Scalar(0,200,0));
-			setHSVmax(new Scalar(19,255,255));
+//			setHSVmin(new Scalar(0,200,0));
+//			setHSVmax(new Scalar(19,255,255));
+
+			setHSVmin(new Scalar(132,127,78));
+			setHSVmax(new Scalar(180,255,113));
 
 			//BGR value for Red:
 			setColor(new Scalar(0,0,255,255));
